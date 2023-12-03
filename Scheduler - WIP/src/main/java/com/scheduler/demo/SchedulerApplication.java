93c85077b5a8480a7568955c649209d02781ec11
Kongtanker <147048540+Kongtanker@users.noreply.github.com>
2023-12-03 02:24:56 -0800

    Database Integration WIP
    Currently, only the user function is operational.
    Refer to the other skeleton for a more "complete" code

---
+++ Scheduler - WIP/src/main/java/com/scheduler/demo/SchedulerApplication.java
@@ -0,0 +1,158 @@
+package com.scheduler.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.*;
+
+@SpringBootApplication
+@RestController
+public class SchedulerApplication {
+	private int id = 0;
+	@Autowired
+	private UserRep userRep;
+	
+    public static void main(String[] args) {
+      SpringApplication.run(SchedulerApplication.class, args);
+    }
+    
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+    
+    // List starts out empty
+    @GetMapping("/api/user")
+    public Iterable<User> get_all_user() {
+    	return userRep.findAll();
+    }
+    
+    @GetMapping ("/api/user/{user_id}")
+    public Optional<User> get_one_user(@PathVariable(value = "user_id")int Id) {
+    	return userRep.findById(Id);
+    }
+    
+    @PostMapping ("/api/user")
+    public @ResponseBody void add_user(@RequestBody User user) {
+    	user.setId(id);
+    	userRep.save(user);
+    	id++;
+    }
+    
+    @PutMapping ("/api/user/{user_id}")
+    public void edit_user(@PathVariable(value = "user_id") int Id,@RequestBody User user) {
+    	User oldUser = userRep.findById(Id).get();
+    	oldUser.setName(user.getName());
+    	oldUser.setAge(user.getAge());
+    	oldUser.setMajor(user.getMajor());
+    	userRep.save(oldUser);
+    }
+
+    @DeleteMapping ("/api/user/{user_id}")
+    public void delete_user(@PathVariable(value = "user_id") int Id) {
+    	userRep.deleteById(Id);
+    }
+    
+    @GetMapping ("/api/user/{user_id}/availability")
+    public List<Time> get_time(@PathVariable(value = "user_id")int Id) {
+    	ArrayList<Time> tempList = new ArrayList<Time>();
+    	for(int i = 0; i < timeList.size(); i++) {
+    		if (timeList.get(i).getUserID() == Id) {
+    	    	tempList.add(timeList.get(i));
+    		}
+    	}
+    	return tempList;
+    }
+    
+    @PostMapping ("/api/user/{user_id}/availability")
+    public void add_time(@PathVariable(value = "user_id")int Id, @RequestBody Time time) {
+    	time.setTimeID(time_id);
+    	time.setUserID(Id);
+    	timeList.add(time);
+    	time_id++;
+    }
+    
+    @PutMapping ("/api/user/{user_id}/availability/{availability_id}")
+    public void edit_time(@PathVariable(value = "user_id")int Id, @PathVariable(value = "availability_id")int Time_Id, @RequestParam int start, @RequestParam int end) {
+    	for(int i = 0; i <timeList.size(); i++) {
+    		if(timeList.get(i).getTimeID() == Time_Id) {
+    			timeList.get(i).setStart(start);
+				timeList.get(i).setEnd(end);
+    		}
+    	}
+    }
+    
+    @DeleteMapping ("/api/user/{user_id}/availability/{availability_id}")
+    public void delete_time(@PathVariable(value = "user_id")int Id, @PathVariable(value = "availability_id")int Time_Id) {
+    	for(int i = 0; i <timeList.size(); i++) {
+    		if(timeList.get(i).getTimeID() == Time_Id) {
+    			timeList.remove(i);
+    		}
+    	}
+    }
+    @GetMapping ("/api/meeting")
+    public List<Meeting> get_meeting() {
+    	return meetingList;
+    }
+    
+    @GetMapping ("/api/meeting/{meeting_id}")
+    public Meeting get_one_meeting(@PathVariable(value = "meeting_id")int Id) {
+    	return meetingList.get(Id);
+    }
+    
+    @PostMapping ("/api/meeting")
+    public void add_meeting(@RequestBody Meeting meeting) {
+    	meeting.setMeetingId(meeting_id);
+    	meetingList.add(meeting);
+    	meeting_id++;
+    }
+    
+    @PutMapping ("/api/meeting/{meeting_id}")
+    public void edit_meeting(@PathVariable(value = "meeting_id")int Id, @RequestParam int start, @RequestParam int end) {
+    	for(int i = 0; i <meetingList.size(); i++) {
+    		if(meetingList.get(i).getMeetingID() == Id) {
+    			meetingList.get(i).setMeetingStart(start);
+				meetingList.get(i).setMeetingEnd(end);
+    		}
+    	}
+    }
+    
+    @DeleteMapping ("/api/meeting/{meeting_id}")
+    public void delete_meeting(@PathVariable(value = "meeting_id")int Id) {
+    	for(int i = 0; i <meetingList.size(); i++) {
+    		if(meetingList.get(i).getMeetingID() == Id) {
+    			meetingList.remove(i);
+    		}
+    	}
+    }
+    
+    //Support up to 4 users, more could be implemented
+    @GetMapping ("/api/timeslots")
+    public Time get_timeslots(@RequestParam int id1, @RequestParam(required = false) Integer id2, @RequestParam(required = false) Integer id3, @RequestParam(required = false) Integer id4) {
+    	int timeSlotStart = 0;
+    	int timeSlotEnd = 24;
+    	
+    	for(int i = 0; i < timeList.size(); i++) {
+    		if (timeList.get(i).getUserID() == id1 || (id2 != null &&timeList.get(i).getUserID() == id2) || (id3 != null &&timeList.get(i).getUserID() == id3) || (id4 != null &&timeList.get(i).getUserID() == id4)) {
+    	    	if (timeList.get(i).getStart() > timeSlotStart) {
+    	    		timeSlotStart = timeList.get(i).getStart();
+    	    	}
+    	    	if (timeList.get(i).getEnd() < timeSlotEnd) {
+    	    		timeSlotEnd = timeList.get(i).getEnd();
+    	    	}
+    		}
+    	}
+    	if (timeSlotStart > timeSlotEnd) {
+    		return new Time("No Time Slot Available");
+    	}
+    	return new Time(timeSlotStart, timeSlotEnd);
+    }
+}
---
+++ Scheduler - WIP/src/main/java/com/scheduler/demo/Time.java
@@ -0,0 +1,63 @@
+package com.scheduler.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Time{
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	
+	private int start;
+	private int end;
+	private int time_id;
+	private int user_id;
+	private String message;
+	
+	public Time(){
+		
+	}
+	
+	public Time(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	public Time(String message) {
+		this.message = message;
+	}
+	
+	public Time(int start, int end, int time_id, int user_id) {
+		this.start = start;
+		this.end = end;
+		this.time_id = time_id;
+		this.user_id = user_id;
+	}
+
+	public int getStart() {
+		return start;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public int getTimeID() {
+		return time_id;
+	}
+	public int getUserID() {
+		return user_id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public void setTimeID(int time_id) {
+		this.time_id = time_id;
+	}
+	public void setUserID(int user_id) {
+		this.user_id = user_id;
+	}
+}
