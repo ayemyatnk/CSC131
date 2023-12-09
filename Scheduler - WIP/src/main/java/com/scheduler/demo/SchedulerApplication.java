1a8985168f1b99358f2fa080b806127bc1ebfc22
Spencer Headspeth <spencerheadspeth@Spencers-MacBook-Air.local>
2023-12-09 03:06:18 -0800

    added simple queries

--- Scheduler - WIP/src/main/java/com/scheduler/demo/SchedulerApplication.java
+++ Scheduler - WIP/src/main/java/com/scheduler/demo/SchedulerApplication.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Service;
+
 import java.util.*;
 
 @SpringBootApplication
--- Scheduler - WIP/src/main/java/com/scheduler/demo/UserRep.java
+++ Scheduler - WIP/src/main/java/com/scheduler/demo/UserRep.java
@@ -1,9 +1,12 @@
 package com.scheduler.demo;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
-
+import java.util.List;
+import java.util.Optional;
 import com.scheduler.demo.User;
 
 public interface UserRep extends CrudRepository<User, Integer>{
+    Optional<User> findById(long user_id);
 
 }
