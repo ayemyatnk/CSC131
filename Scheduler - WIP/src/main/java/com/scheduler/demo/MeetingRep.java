85e150cfb1799bc70c2f59c8e25e00696781acd2
Kongtanker <147048540+Kongtanker@users.noreply.github.com>
2023-12-13 03:53:15 -0800

    Delete Scheduler - WIP directory
    No longer necessary

--- Scheduler - WIP/src/main/java/com/scheduler/demo/MeetingRep.java
+++
@@ -1,9 +0,0 @@
-package com.scheduler.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.scheduler.demo.Meeting;
-
-public interface MeetingRep extends CrudRepository<Meeting, Integer>{
-
-}
--- Scheduler - WIP/src/main/java/com/scheduler/demo/TimeRep.java
+++
@@ -1,9 +0,0 @@
-package com.scheduler.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.scheduler.demo.Time;
-
-public interface TimeRep extends CrudRepository<Time, Integer>{
-    Iterable<Time> findByUserId(Integer user_id);
-}
